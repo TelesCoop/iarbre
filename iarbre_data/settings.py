2e22c3e133a93cbd38f314ae0ea59f96304a3430
Kout95 <antoine.bernier@outlook.fr>
2025-01-07 18:08:09 +0100

    fix: settings

--- iarbre_data/settings.py
+++ iarbre_data/settings.py
@@ -177,17 +177,19 @@ if IS_LOCAL_DEV:
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
     MEDIA_URL = "media/"
 else:
-    # STATIC
-    STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"
+        },
+    }
     STATIC_ROOT = config.getstr("staticfiles.static_root")
-
-    # MEDIA
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     AWS_S3_ACCESS_KEY_ID = config.getstr("external_file_storage.access")
     AWS_S3_SECRET_ACCESS_KEY = config.getstr("external_file_storage.secret")
     AWS_STORAGE_BUCKET_NAME = config.getstr("external_file_storage.bucket")
     AWS_S3_SIGNATURE_VERSION = "s3v4"
-    # AWS_DEFAULT_ACL = "private"
     AWS_S3_REGION_NAME = config.getstr("external_file_storage.region")
     AWS_S3_HOST = config.getstr("external_file_storage.host")
     AWS_S3_ENDPOINT_URL = "https://{}".format(AWS_S3_HOST)
