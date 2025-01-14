4e21b9e2ff1cfee6e60d05cf61d6b449fb86b1c2
Maxime Bellec <maxime@telescoop.fr>
2025-01-14 15:04:44 +0100

    up

--- back/api/map.py
+++ back/api/map.py
@@ -84,7 +84,7 @@ def load_tiles(model, x, y, zoom):
     try:
         tile = MVTTile.objects.get(zoom_level=zoom, tile_x=x, tile_y=y)
         return HttpResponse(tile.mvt_file, content_type="application/x-protobuf")
-    except MVTTile.DoesNotExist:
+    except (MVTTile.DoesNotExist, FileNotFoundError):
         raise Http404("Tile not found")
 
 
