0d5290b7432c18bff8bda72f6fd344bcd520cc05
Ludo <ludovic.darmet@gmail.com>
2025-02-18 10:39:26 +0100

    front: fix: local variable

--- front/src/utils/constants.ts
+++ front/src/utils/constants.ts
@@ -2,9 +2,11 @@ export enum Layout {
   Default = "Default"
 }
 
-export const FULL_BASE_API_URL = import.meta.env.PROD
-  ? `${window.location.origin}/api`
-  : "http://localhost:8000/api"
+const IS_LOCAL_DEV = import.meta.env.TELESCOOP_DEV === 1
+
+export const FULL_BASE_API_URL = IS_LOCAL_DEV
+  ? "http://localhost:8000/api"
+  : import.meta.env.VITE_BASE_API_URL || `${window.location.origin}/api`
 
 export const MIN_ZOOM = 12
 export const MAX_ZOOM = 20
