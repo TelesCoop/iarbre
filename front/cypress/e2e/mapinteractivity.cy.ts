edd80c01ffa20911f53a3d9db8dcaa6a2720f73c
Marc-AntoineA <dev@marc-antoinea.fr>
2025-02-27 14:45:40 +0100

    test: fix

--- front/cypress/e2e/mapinteractivity.cy.ts
+++ front/cypress/e2e/mapinteractivity.cy.ts
@@ -8,10 +8,11 @@ describe("Map interactivity", () => {
   it("Map loading seems to be okay", () => {
     cy.getBySel("plantability-legend").should("exist")
     cy.getBySel("map-component").should("exist")
+    cy.contains("OpenStreetMap Contributors").should("exist")
   })
   it.only("Opens popup on click", () => {
     // eslint-disable-next-line
-    cy.wait(2000)
+    cy.wait(4000)
     cy.getBySel("map-component").click("center")
     cy.getBySel("score-popup").should("be.visible")
   })
