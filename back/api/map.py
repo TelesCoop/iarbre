f8e824dfbb20ab173f077f07ee0825dcaa0e0d76
Ludo <ludovic.darmet@gmail.com>
2025-02-27 14:26:17 +0100

    up

--- back/api/map.py
+++ back/api/map.py
@@ -7,7 +7,9 @@ from iarbre_data.models import MVTTile
 @lru_cache(maxsize=1024)
 def load_tiles(model, x, y, zoom):
     try:
-        tile = MVTTile.objects.get(model_type=model, zoom_level=zoom, tile_x=x, tile_y=y)
+        tile = MVTTile.objects.get(
+            model_type=model, zoom_level=zoom, tile_x=x, tile_y=y
+        )
         return HttpResponse(tile.mvt_file, content_type="application/x-protobuf")
     except (MVTTile.DoesNotExist, FileNotFoundError):
         raise Http404("Tile not found")
