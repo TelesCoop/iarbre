3886cb6d422eef323ab2ebaebfae67bbe283f655
Ludo <ludovic.darmet@gmail.com>
2025-03-04 09:48:49 +0100

    add source code link

--- front/src/views/MapView.vue
+++ front/src/views/MapView.vue
@@ -5,6 +5,9 @@ import MapComponent from "@/components/map/MapComponent.vue"
 <template>
   <div class="map-container max-full-height">
     <map-component map-id="default" class="h-100" />
+    <a href="https://github.com/TelesCoop/iarbre" target="_blank" class="code-source-link"
+      >code_source</a
+    >
   </div>
 </template>
 
@@ -14,4 +17,16 @@ import MapComponent from "@/components/map/MapComponent.vue"
   height:  $content-height
   overflow: hidden
   position: relative
+
+.code-source-link
+  position: absolute
+  bottom: 1rem
+  left: 1.1rem
+  color: black
+  text-decoration: underline
+  font-size: 1.2rem
+  font-weight: bold
+  background-color: rgba(239, 239, 237, 0.6)  // Semi-transparent $off-white
+  padding: 0.2rem 0.5rem
+  border-radius: 5px
 </style>
