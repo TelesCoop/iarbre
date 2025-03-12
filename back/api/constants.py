f9b45bdb2a4609bd7abcb37ea023188b6b22a299
Ludo <ludovic.darmet@gmail.com>
2025-03-12 16:56:41 +0100

    chore(back) remove useless enum

--- back/api/constants.py
+++ back/api/constants.py
@@ -1,15 +1,1 @@
-from django.db.models import TextChoices
-
 DEFAULT_ZOOM_LEVELS = (12, 16)
-
-
-class GeoLevel(TextChoices):
-    TILE = "tile", "Tile"
-    CITY = "city", "City"
-    IRIS = "iris", "Iris"
-    LCZ = "lcz", "LCZ"
-
-
-class DataType(TextChoices):
-    LCZ = "lcz", "LCZ"
-    TILE = "plantability", "Plantability"
