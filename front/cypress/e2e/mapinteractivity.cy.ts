edc20441307b5ddda112eb4b8334710d99cecf9f
Marc-AntoineA <dev@marc-antoinea.fr>
2025-03-24 14:54:07 +0100

    fix(front): popup can be reopened on layer switch
    The popup dom element must be captured only once.

--- front/cypress/e2e/mapinteractivity.cy.ts
+++ front/cypress/e2e/mapinteractivity.cy.ts
@@ -4,6 +4,7 @@ describe("Map interactivity", () => {
   beforeEach(() => {
     cy.visit("/")
     cy.get("@consoleInfo").should("have.been.calledWith", "cypress: map data loaded")
+    cy.wait(200) // eslint-disable-line cypress/no-unnecessary-waiting
   })
 
   it("Map loading seems to be okay", () => {
@@ -11,20 +12,22 @@ describe("Map interactivity", () => {
     cy.getBySel("map-component").should("exist")
     cy.contains("OpenStreetMap Contributors").should("exist")
   })
-  it.skip("Open popup on click", () => {
-    cy.getBySel("map-component").click("center")
-    cy.getBySel("score-popup").should("be.visible")
+  it("Open popup on click", () => {
+    cy.openPopup()
   })
 
-  it.skip("Open, case and reopen popup. Resolve #92", () => {
-    cy.getBySel("map-component").click("center")
-    cy.getBySel("score-popup").should("be.visible")
+  it("Open, close and reopen popup. Resolve #92", () => {
+    cy.openPopup()
+    cy.closePopup()
+    cy.openPopup()
+  })
 
-    // This might be behind the legend
-    cy.get(".maplibregl-popup-close-button").click({ force: true })
-    cy.getBySel("score-popup").should("not.exist")
-
-    cy.getBySel("map-component").click("center")
-    cy.getBySel("score-popup").should("be.visible")
+  it.only("Open popup, close it, switch layer and reopen popup. Resolve #142", () => {
+    cy.openPopup()
+    cy.closePopup()
+    cy.switchLayer("lcz")
+    cy.get("@consoleInfo").should("have.been.calledWith", "cypress: map data loaded")
+    cy.wait(200) // eslint-disable-line cypress/no-unnecessary-waiting
+    cy.openPopup()
   })
 })
--- front/cypress/support/commands.ts
+++ front/cypress/support/commands.ts
@@ -2,3 +2,17 @@
 Cypress.Commands.add("getBySel", (selector, ...args) => {
   return cy.get(`[data-cy=${selector}]`, ...args)
 })
+
+Cypress.Commands.add("openPopup", () => {
+  cy.getBySel("map-component").click("center")
+  cy.getBySel("score-popup").should("be.visible")
+})
+
+Cypress.Commands.add("closePopup", () => {
+  cy.get(".maplibregl-popup-close-button").click({ force: true })
+  cy.getBySel("score-popup").should("not.exist")
+})
+
+Cypress.Commands.add("switchLayer", (datatype: string) => {
+  cy.getBySel("layer-switcher").get("select").select(datatype)
+})
