73ba801f8973948accb07e210015d5f5f6a98da6
Quentin Madura <quentin.madura@gmail.com>
2025-04-23 15:46:51 +0200

    adds primevue

--- front/src/components/FeedbackPopin.vue
+++ front/src/components/FeedbackPopin.vue
@@ -1,52 +1,30 @@
-<script setup lang="ts">
-import { ref } from "vue"
+<script lang="ts" setup>
+import { ref, defineModel } from "vue"
 import FeedbackForm from "@/components/forms/FeedbackForm.vue"
+import Dialog from "primevue/dialog"
 
-const props = defineProps({
-  message: {
-    required: true,
-    type: String
-  }
+const visible = defineModel({
+  type: Boolean,
+  required: true
 })
-
 const email = ref("")
 const feedback = ref("")
-
 const emit = defineEmits(["submit-feedback", "close"])
 </script>
 
 <template>
-  <div class="popin" data-cy="feedback-popin">
-    <button class="popin-close-button" data-cy="close-feedback-button" @click="emit('close')">
-      x
-    </button>
-    <h3 class="popin-heading">Votre avis compte !</h3>
-    <p class="popin-text">Partagez-nous vos impressions pour nous aider à améliorer le site :</p>
+  <Dialog
+    v-model:visible="visible"
+    :draggable="false"
+    :style="{ width: '25rem' }"
+    header="Votre avis compte !"
+    modal
+    @hide="emit('close')"
+  >
     <feedback-form
       :email="email"
       :feedback="feedback"
-      :message="props.message"
       @submit-feedback="emit('submit-feedback', { email: $event.email, feedback: $event.feedback })"
     />
-  </div>
+  </Dialog>
 </template>
-
-<style scoped>
-@reference "@/styles/main.css";
-
-.popin {
-  @apply fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 max-w-sm mx-auto p-5 border border-gray-300 rounded-lg bg-white text-dark-green text-left flex flex-col text-sm box-border;
-}
-
-.popin-close-button {
-  @apply absolute top-2 right-2 text-lg text-dark-green cursor-pointer transition-colors duration-300 hover:text-red-500;
-}
-
-.popin-heading {
-  @apply font-medium;
-}
-
-.popin-text {
-  @apply mb-2;
-}
-</style>
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,14 +1,129 @@
 import "./styles/main.css"
-
+import PrimeVue from "primevue/config"
+import Aura from "@primeuix/themes/aura"
+import { definePreset } from "@primeuix/themes"
 import { createApp } from "vue"
 import { createPinia } from "pinia"
+import ToastService from "primevue/toastservice"
 
 import App from "./App.vue"
 import router from "./router"
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
+
+const defaultPadding = {
+  x: "1.5rem",
+  y: "0.625rem"
+}
+
+const defaultBorder = {
+  radius: "20px",
+  color: "#BCBCBC"
+}
+
+const defaultPlaceholder = {
+  color: "{primary.500}"
+}
+
+const defaultDropdown = {
+  color: "{primary.500}"
+}
+
+const lightPrimaryColors = {
+  color: "{primary.500}",
+  inverseColor: "#ffffff",
+  hoverColor: "{primary.600}",
+  activeColor: "{primary.700}"
+}
+
+const lightSecondaryColors = {
+  color: "{primary.500}",
+  inverseColor: "#ffffff",
+  hoverColor: "{secondary.600}",
+  activeColor: "{secondary.800}"
+}
+
+const IArbre = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "#EDF5E9",
+      100: "#C7DBC0",
+      200: "#97B090",
+      300: "#7A9374",
+      400: "#6D8766",
+      500: "#426A45",
+      600: "#49673F",
+      700: "#34522A",
+      800: "#2B4822",
+      900: "#002814"
+    },
+    colorScheme: {
+      light: {
+        primary: lightPrimaryColors,
+        secondary: lightSecondaryColors
+      },
+      dark: {}
+    }
+  },
+  components: {
+    select: {
+      // @ts-ignore
+      padding: defaultPadding,
+      border: defaultBorder,
+      placeholder: defaultPlaceholder,
+      dropdown: defaultDropdown,
+      color: "{primary.500}"
+    },
+    inputtext: {
+      // @ts-ignore
+      padding: defaultPadding,
+      border: defaultBorder,
+      placeholder: defaultPlaceholder,
+      dropdown: defaultDropdown
+    },
+    textarea: {
+      // @ts-ignore
+      padding: defaultPadding,
+      border: defaultBorder,
+      placeholder: defaultPlaceholder
+    },
+    button: {
+      padding: defaultPadding,
+      border: { radius: "20px" },
+      colorScheme: {
+        light: {
+          // @ts-ignore
+          primary: {
+            background: "white",
+            color: "{primary.500}",
+            hoverColor: "{primary.500}",
+            hoverBackground: "{gray.100}",
+            hoverBorderColor: "#BCBCBC",
+            border: {
+              color: "#BCBCBC"
+            }
+          },
+          secondary: {
+            background: "{primary.500}",
+            hoverBackground: "{primary.700}",
+            activeBackground: "{primary.800}",
+            color: "white",
+            hoverColor: "white",
+            activeColor: "white"
+          }
+        }
+      }
+    }
+  }
+})
+
+app.use(PrimeVue, {
+  theme: {
+    preset: IArbre
+  }
+})
+app.use(ToastService)
 
 app.mount("#app")
