5ccd4694dcf2d6e046b0af3dc3c58d8f980c0bfb
Ludo <ludovic.darmet@gmail.com>
2025-04-23 13:28:18 +0200

    front: update min zoom

--- front/src/utils/constants.ts
+++ front/src/utils/constants.ts
@@ -2,6 +2,6 @@ export enum Layout {
   Default = "Default"
 }
 
-export const MIN_ZOOM = 12
+export const MIN_ZOOM = 10
 export const MAX_ZOOM = 20
 export const MAP_CONTROL_POSITION = "bottom-right"
