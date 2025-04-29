90089e0cf8ee4b222cbc18168b2517b62f964274
Quentin Madura <quentin.madura@gmail.com>
2025-04-29 14:18:54 +0200

    update test

--- front/src/components/map/legend/MapLegend.vue
+++ front/src/components/map/legend/MapLegend.vue
@@ -9,9 +9,7 @@ const mapStore = useMapStore()
 </script>
 
 <template>
-  <div
-    class="z-3 absolute max-lg:bottom-18 max-lg:left-4 lg:top-4 lg:right-4 flex flex-col items-center bg-white p-3 rounded-md border-primary-500 border-1 shadow-lg"
-  >
+  <div class="map-legend">
     <div class="mb-2 text-sm font-semibold font-accent text-primary-900" data-cy="map-legend-title">
       {{ DataTypeToLabel[mapStore.selectedDataType] }}
     </div>
@@ -20,3 +18,16 @@ const mapStore = useMapStore()
     <vulnerability-legend v-else-if="mapStore.selectedDataType === DataType.VULNERABILITY" />
   </div>
 </template>
+
+<style scoped>
+@reference "@/styles/main.css";
+.map-legend {
+  @apply bg-white;
+  @apply p-3;
+  @apply absolute z-3;
+  @apply max-lg:bottom-18 max-lg:left-4 lg:top-4 lg:right-4;
+  @apply flex flex-col items-center;
+  @apply rounded-md border-primary-500 border-1;
+  @apply shadow-lg;
+}
+</style>
