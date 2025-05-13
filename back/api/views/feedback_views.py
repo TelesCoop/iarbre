e1b0fd262fa6add9fe204e803df4fae64c6eaa3a
Ludo <ludovic.darmet@gmail.com>
2025-05-13 10:48:02 +0200

    typo

--- back/api/views/feedback_views.py
+++ back/api/views/feedback_views.py
@@ -43,7 +43,7 @@ class FeedbackView(generics.CreateAPIView):
         """
         try:
             response = requests.post(
-                f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
+                f"https://api.eu.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
                 auth=("api", f"{MAILGUN_API_KEY}"),
                 data={
                     "from": f"Feedback IArbre <postmaster@{MAILGUN_DOMAIN}>",
