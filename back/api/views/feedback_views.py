d367c5cc19ffafb713d103378c2bb956857f36f9
Ludovic de Clermont <ludovicdmt@users.noreply.github.com>
2025-05-14 12:36:29 +0200

    feat(back): send feedbacks by emails (#233)

--- back/api/views/feedback_views.py
+++ back/api/views/feedback_views.py
@@ -1,9 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.core.mail import send_mail
 
 from api.models import Feedback
 from api.serializers import FeedbackSerializer
+
+from django.conf import settings
 
 
 class FeedbackView(generics.CreateAPIView):
@@ -12,4 +15,33 @@ class FeedbackView(generics.CreateAPIView):
 
     @method_decorator(ensure_csrf_cookie)
     def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        response = self.create(request, *args, **kwargs)
+
+        if response.status_code == status.HTTP_201_CREATED:
+            self.send_feedback_email(response.data)
+
+        return response
+
+    def send_feedback_email(self, feedback_data):
+        """Send feedback data via email."""
+
+        email = feedback_data.get("email", "Anonyme")
+        subject = f"Retour utilisateur IA.rbre de {email}"
+        created_date = feedback_data["created"]
+
+        email_body = f"""
+        # Retour Utilisateur
+
+        **Email**: {email}
+        **Date**: {created_date}
+
+        ## Commentaire :
+        {feedback_data['feedback']}
+        """
+
+        send_mail(
+            subject,
+            email_body,
+            settings.DEFAULT_FROM_EMAIL,
+            [settings.RECIPIENT_EMAIL],
+        )
