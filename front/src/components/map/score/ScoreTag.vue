a5e488046431fde9ed4763c5370e05658582da70
Kout95 <antoine.bernier@outlook.fr>
2025-06-30 11:34:07 +0200

    fix: score overflow in mobile

--- front/src/components/map/score/ScoreTag.vue
+++ front/src/components/map/score/ScoreTag.vue
@@ -5,15 +5,27 @@ defineProps<{
   total: number
   backgroundColor: string
   customSizeClass?: string
+  isVertical?: boolean
 }>()
 </script>
 
 <template>
   <span
     :style="{ backgroundColor: backgroundColor }"
-    class="rounded flex justify-center items-center"
-    :class="[getAdaptativeColorClass(backgroundColor), customSizeClass ?? 'w-14 h-8 p-2']"
+    class="rounded flex justify-center items-center gap-1"
+    :class="[
+      getAdaptativeColorClass(backgroundColor),
+      customSizeClass ?? 'w-14 h-8 p-2',
+      isVertical ? 'flex-col' : 'flex-row'
+    ]"
   >
-    {{ score }} / {{ total }}
+    <div>{{ score }}</div>
+    <div
+      v-if="isVertical"
+      class="w-4 border-1"
+      :class="getAdaptativeColorClass(backgroundColor, 'border-')"
+    ></div>
+    <div v-else>/</div>
+    <div>{{ total }}</div>
   </span>
 </template>
