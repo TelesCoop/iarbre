d76791568ed546fe690867a0a8ab525418aefc3b
Ludo <ludovic.darmet@gmail.com>
2025-08-27 15:37:45 +0200

    Update

--- front/src/components/map/panels/MapSidePanel.vue
+++ front/src/components/map/panels/MapSidePanel.vue
@@ -7,11 +7,11 @@ import MapSidePanelHeader from "./MapSidePanelHeader.vue"
     class="absolute bg-white lg:flex top-0 left-0 flex-col z-1 w-[20rem] md:w-[24rem] lg:w-[26rem] xl:w-[30rem] 2xl:w-[32rem] max-w-[calc(100vw-0.5rem)]"
   >
     <map-side-panel-header />
-    <div class="m-5">
+    <div class="mx-5">
       <map-layer-switcher />
       <map-bg-switcher />
       <map-context-data
-        class="max-h-[calc(100vh-6rem)] sm:max-h-[calc(100vh-7rem)] lg:max-h-[calc(100vh-6rem)] p-0!"
+        class="max-h-[calc(100vh-6rem)] sm:max-h-[calc(100vh-7rem)] lg:max-h-[calc(100vh-6rem)] py-4!"
       />
     </div>
   </div>
