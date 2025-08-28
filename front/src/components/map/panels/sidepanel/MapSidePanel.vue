6007765b18930489e6115705de54855a70a8a3ea
Ludo <ludovic.darmet@gmail.com>
2025-08-28 11:20:46 +0200

    up

--- front/src/components/map/panels/sidepanel/MapSidePanel.vue
+++ front/src/components/map/panels/sidepanel/MapSidePanel.vue
@@ -8,7 +8,7 @@ import MapSidePanelFooter from "./MapSidePanelFooter.vue"
     class="absolute bg-white lg:flex top-0 left-0 flex-col z-1 w-[20rem] md:w-[24rem] lg:w-[26rem] xl:w-[30rem] 2xl:w-[32rem] max-w-[calc(100vw-0.5rem)] h-full"
   >
     <map-side-panel-header />
-    <div class="mx-5 flex-1 overflow-y-auto space-y-3 items-center">
+    <div class="mx-5 flex-1 overflow-y-auto space-y-3 items-center py-4">
       <map-bg-switcher />
       <map-layer-switcher />
       <map-context-data
