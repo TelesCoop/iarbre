2c40b64b31736378ab1b95cbee92887537c6b415
Ludo <ludovic.darmet@gmail.com>
2025-09-01 10:24:26 +0200

    up

--- front/src/components/map/panels/sidepanel/MapSidePanel.vue
+++ front/src/components/map/panels/sidepanel/MapSidePanel.vue
@@ -4,9 +4,7 @@
     <div class="mx-5 flex-1 overflow-y-auto space-y-3 items-center py-4">
       <map-bg-switcher />
       <map-layer-switcher />
-      <map-context-data
-        class="max-h-[calc(100vh-6rem)] sm:max-h-[calc(100vh-7rem)] lg:max-h-[calc(100vh-6rem)] py-4!"
-      />
+      <map-context-data />
       <map-side-panel-footer />
     </div>
   </div>
