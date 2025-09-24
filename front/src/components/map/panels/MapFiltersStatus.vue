b40e546146aeaa411d2a22b39731cef4015316a7
Ludo <ludovic.darmet@gmail.com>
2025-09-24 09:28:37 +0200

    fix(front) : filter status UI

--- front/src/components/map/panels/MapFiltersStatus.vue
+++ front/src/components/map/panels/MapFiltersStatus.vue
@@ -15,20 +15,22 @@ const filterSummary = computed(() => {
 </script>
 
 <template>
-  <div v-if="mapStore.hasActiveFilters" class="map-tool-container" data-cy="map-filters-status">
-    <div class="flex items-center justify-between gap-3 w-full">
-      <div class="flex items-center gap-2 flex-1">
-        <span class="font-medium text-xs">Filtres&nbsp;:</span>
-        <Chip
-          v-if="nbFilters > 0"
-          :label="filterSummary"
-          class="filter-summary-chip"
-          data-cy="filter-summary"
-          outlined
-          severity="info"
-          size="small"
-        ></Chip>
-      </div>
+  <div
+    v-if="mapStore.hasActiveFilters"
+    class="bg-white flex px-4 py-2 justify-center items-center gap-2 rounded-lg mb-2"
+    data-cy="map-filters-status"
+  >
+    <div class="flex items-center gap-2 w-full justify-center">
+      <span class="font-medium text-xs">Filtres&nbsp;:</span>
+      <Chip
+        v-if="nbFilters > 0"
+        :label="filterSummary"
+        class="filter-summary-chip"
+        data-cy="filter-summary"
+        outlined
+        severity="info"
+        size="small"
+      ></Chip>
       <Button
         :icon-class="'filter-reset-icon'"
         data-cy="reset-filters-button"
