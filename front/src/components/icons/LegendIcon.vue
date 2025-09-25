0405d5148547348554b5d2639526c52ee3a8ecb0
Ludo <ludovic.darmet@gmail.com>
2025-09-25 10:51:41 +0200

    colors with plantability.ts

--- front/src/components/icons/LegendIcon.vue
+++ front/src/components/icons/LegendIcon.vue
@@ -1,11 +1,13 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { PLANTABILITY_COLOR_MAP } from "@/utils/plantability"
+</script>
 <template>
-  <svg viewBox="0 0 30 17" width="45">
-    <rect x="0" y="7" width="5" height="35" fill="#C4C4C4" />
-    <rect x="5" y="7" width="5" height="35" fill="#BF5A16" />
-    <rect x="10" y="7" width="5" height="35" fill="#DDAD14" />
-    <rect x="15" y="7" width="5" height="35" fill="#A6CC4A" />
-    <rect x="20" y="7" width="5" height="35" fill="#55B250" />
-    <rect x="25" y="7" width="5" height="35" fill="#025400" />
+  <svg viewBox="0 0 30 20" width="45">
+    <rect x="0" y="7" width="5" height="35" :fill="PLANTABILITY_COLOR_MAP[1] as string" />
+    <rect x="5" y="7" width="5" height="35" :fill="PLANTABILITY_COLOR_MAP[5] as string" />
+    <rect x="10" y="7" width="5" height="35" :fill="PLANTABILITY_COLOR_MAP[9] as string" />
+    <rect x="15" y="7" width="5" height="35" :fill="PLANTABILITY_COLOR_MAP[13] as string" />
+    <rect x="20" y="7" width="5" height="35" :fill="PLANTABILITY_COLOR_MAP[17] as string" />
+    <rect x="25" y="7" width="5" height="35" :fill="PLANTABILITY_COLOR_MAP[21] as string" />
   </svg>
 </template>
