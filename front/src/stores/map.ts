16d6ef091a487984a2a1be04772e3a940e553a06
Ludo <ludovic.darmet@gmail.com>
2025-09-30 12:13:47 +0200

    Add PlantabilityVulnerability props

--- front/src/stores/map.ts
+++ front/src/stores/map.ts
@@ -188,20 +188,14 @@ export const useMapStore = defineStore("map", () => {
       const featureId = extractFeatureProperty(e.features!, datatype, geolevel, "id")
       const score = extractFeatureProperty(e.features!, datatype, geolevel, "indice")
       const source_values = extractFeatureProperty(e.features!, datatype, geolevel, "source_values")
-      if (geolevel === GeoLevel.TILE && datatype === DataType.PLANT_VULNERABILITY) {
-        const vuln_score_day = extractFeatureProperty(
-          e.features!,
-          datatype,
-          geolevel,
-          "vulnearability_indice_day"
-        )
-        const vuln_score_night = extractFeatureProperty(
-          e.features!,
-          datatype,
-          geolevel,
-          "vulnearability_indice_night"
-        )
-      }
+      const vuln_score_day =
+        geolevel === GeoLevel.TILE && datatype === DataType.PLANT_VULNERABILITY
+          ? extractFeatureProperty(e.features!, datatype, geolevel, "vulnerability_indice_day")
+          : undefined
+      const vuln_score_night =
+        geolevel === GeoLevel.TILE && datatype === DataType.PLANT_VULNERABILITY
+          ? extractFeatureProperty(e.features!, datatype, geolevel, "vulnerability_indice_night")
+          : undefined
       highlightFeature(map, layerId, featureId)
       // Store click coordinates
       clickCoordinates.value = {
@@ -212,7 +206,7 @@ export const useMapStore = defineStore("map", () => {
       if (geolevel === GeoLevel.TILE && datatype === DataType.PLANTABILITY && map.getZoom() < 17) {
         contextData.setData(featureId, score, source_values)
       } else if (geolevel === GeoLevel.TILE && datatype === DataType.PLANT_VULNERABILITY) {
-        contextData.setData(featureId, score, source_values)
+        contextData.setData(featureId, score, source_values, vuln_score_day, vuln_score_night)
       } else {
         contextData.setData(featureId)
       }
