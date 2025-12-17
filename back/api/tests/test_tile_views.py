f576e89d22c352310145748ea5cf2badf93f9164
Ludo <ludovic.darmet@gmail.com>
2025-12-17 15:09:52 +0100

    Add test for the new view

--- back/api/tests/test_tile_views.py
+++ back/api/tests/test_tile_views.py
@@ -1,3 +1,4 @@
+import json
 from django.test import TestCase, Client
 from django.core.files.base import ContentFile
 from django.contrib.gis.geos import Polygon
@@ -93,7 +94,6 @@ class TileDetailsViewTest(TestCase):
         )
 
     def test_plantability_details_with_invalid_json_string(self):
-        # Create tile with invalid JSON string details
         square = Polygon(((0, 0), (1, 0), (1, 1), (0, 1), (0, 0)), srid=2154)
         tile_with_invalid_json = Tile.objects.create(
             geometry=square, details="invalid json string"
@@ -105,3 +105,108 @@ class TileDetailsViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         response_data = response.json()
         self.assertIsNone(response_data["details"])
+
+
+class ScoresInPolygonViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        lyon_square = Polygon(
+            (
+                (845000, 6525000),
+                (845100, 6525000),
+                (845100, 6525100),
+                (845000, 6525100),
+                (845000, 6525000),
+            ),
+            srid=2154,
+        )
+        self.tile = Tile.objects.create(
+            geometry=lyon_square, plantability_normalized_indice=7.5
+        )
+        self.vulnerability = Vulnerability.objects.create(
+            geometry=lyon_square,
+            vulnerability_index_day=5.0,
+            vulnerability_index_night=4.0,
+        )
+
+    def test_valid_plantability_polygon(self):
+        url = "/api/tiles/plantability/in-polygon/"
+        polygon = {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [4.867256, 45.809200],
+                    [4.868544, 45.809179],
+                    [4.868574, 45.810079],
+                    [4.867287, 45.810101],
+                    [4.867256, 45.809200],
+                ]
+            ],
+        }
+        response = self.client.post(
+            url, data=json.dumps(polygon), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data["datatype"], "plantability")
+        self.assertIn("count", data)
+        self.assertIn("plantabilityNormalizedIndice", data)
+
+    def test_valid_vulnerability_polygon(self):
+        url = "/api/tiles/vulnerability/in-polygon/"
+        polygon = {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [4.867256, 45.809200],
+                    [4.868544, 45.809179],
+                    [4.868574, 45.810079],
+                    [4.867287, 45.810101],
+                    [4.867256, 45.809200],
+                ]
+            ],
+        }
+        response = self.client.post(
+            url, data=json.dumps(polygon), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data["datatype"], "vulnerability")
+        self.assertIn("vulnerabilityIndiceDay", data)
+
+    def test_invalid_datatype(self):
+        url = "/api/tiles/invalid/in-polygon/"
+        polygon = {"type": "Polygon", "coordinates": [[[0, 0], [1, 0], [1, 1], [0, 0]]]}
+        response = self.client.post(
+            url, data=json.dumps(polygon), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_empty_polygon_data(self):
+        url = "/api/tiles/plantability/in-polygon/"
+        response = self.client.post(url, data="", content_type="application/json")
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_no_tiles_found(self):
+        url = "/api/tiles/plantability/in-polygon/"
+        polygon = {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [2.0, 48.0],
+                    [2.01, 48.0],
+                    [2.01, 48.01],
+                    [2.0, 48.01],
+                    [2.0, 48.0],
+                ]
+            ],
+        }
+        response = self.client.post(
+            url, data=json.dumps(polygon), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 404)
