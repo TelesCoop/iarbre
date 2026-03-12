5092c4e95aabd14c5654075e343cfb07a43aa6dd
Ludo <ludovic.darmet@gmail.com>
2026-03-12 11:52:28 +0100

    Add django-prometheus

--- back/iarbre_data/settings.py
+++ back/iarbre_data/settings.py
@@ -53,6 +53,7 @@ DEBUG = IS_LOCAL_DEV
 # Application definition
 
 INSTALLED_APPS = [
+    "django_prometheus",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -74,6 +75,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django_prometheus.middleware.PrometheusBeforeMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -81,6 +83,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 # Mandatory for Decap CMS Auth
