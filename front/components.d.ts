bd2e5f0dc1790302692dfee40fb517fffe07e3f1
Ludo <ludovic.darmet@gmail.com>
2026-09-01 09:37:19 +0200

    chore(front): remove postcss

--- front/components.d.ts
+++ front/components.d.ts
@@ -143,7 +143,6 @@ declare module 'vue' {
     VulnerabilityMainContextDataScore: typeof import('./src/components/contextData/vulnerability/VulnerabilityMainContextDataScore.vue')['default']
     VulnerabilityScoreLabel: typeof import('./src/components/map/score/VulnerabilityScoreLabel.vue')['default']
     WelcomeMessage: typeof import('./src/components/WelcomeMessage.vue')['default']
-    ZoneDashboardBar: typeof import('./src/components/map/ZoneDashboardBar.vue')['default']
     ZoneDashboardCard: typeof import('./src/components/map/ZoneDashboardCard.vue')['default']
   }
 }
