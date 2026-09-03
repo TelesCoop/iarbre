5acc900703548c2faac9328bebc57320c3286188
Quentin Madura <quentin.madura@gmail.com>
2026-09-03 17:20:48 +0200

    fix: run the health check database probe off the request thread

--- back/api/views/heath_views.py
+++ back/api/views/heath_views.py
@@ -1,13 +1,20 @@
-import time
-import django
+from concurrent.futures import ThreadPoolExecutor
+
+from django.db import connection
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+def _verify_database_connection():
+    try:
+        connection.ensure_connection()
+    finally:
+        connection.close()
+
+
 class HealthCheckView(APIView):
     def get(self, request, *args, **kwargs):
-        # make sure connection to database is working
-        django.db.connection.ensure_connection()
-        # simulate a longer request
-        time.sleep(2)
+        # ensure_connection() must not run on a thread with an event loop.
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            executor.submit(_verify_database_connection).result()
         return Response("OK")
