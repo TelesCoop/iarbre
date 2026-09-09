879d6bd43b6aad237ac488c83d9e735cb1a180c2
Ludo <ludovic.darmet@gmail.com>
2026-09-09 09:22:22 +0200

    fix wrong merge from main

--- back/api/views/heath_views.py
+++ back/api/views/heath_views.py
@@ -1,16 +1,11 @@
 import django
+import time
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-
-def _verify_database_connection():
-    try:
-        connection.ensure_connection()
-    finally:
-        connection.close()
 
 
 class HealthCheckView(APIView):
     def get(self, request, *args, **kwargs):
         django.db.connection.ensure_connection()
+        time.sleep(2)
         return Response("OK")
